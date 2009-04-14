44bbeb3124440a9314c922f79006d1405492918a
c_comte@club-internet.fr <c_comte@club-internet.fr@4dde9020-e581-11dd-a893-8d7ce9fbef62>
2009-04-14 21:38:12 +0000

    Change: finalize nodes to export as references. Copy external imports into same file with attribute "external".

--- trunk/ClassXmlProject/views/XmlImportView.vb
+++ trunk/ClassXmlProject/views/XmlImportView.vb
@@ -361,6 +361,9 @@ Public Class XmlImportView
             Then
                 Dim FileName As System.IO.FileInfo = My.Computer.FileSystem.GetFileInfo(dlgOpenFile.FileName)
                 LoadDocument(form, FileName, eMode)
+
+                ExtractExternalReferences(Me.Node.ParentNode, ChildExportNode().Node)
+
                 ' This class is used both in dlgImport and in frmProject
                 If m_xmlBindingsList IsNot Nothing Then m_xmlBindingsList.ResetValues()
                 bResult = True
