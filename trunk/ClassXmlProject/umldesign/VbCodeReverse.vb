99f8f0edaa0cd1ebb289958a390cab26bd003f17
c_comte@club-internet.fr <c_comte@club-internet.fr@4dde9020-e581-11dd-a893-8d7ce9fbef62>
2009-08-02 12:17:40 +0000

    Pre-implementation of VB Code Reverse - step 2.

--- trunk/ClassXmlProject/umldesign/VbCodeReverse.vb
+++ trunk/ClassXmlProject/umldesign/VbCodeReverse.vb
@@ -5,11 +5,12 @@ Imports Microsoft.VisualBasic
 
 Public Class VbCodeReverse
 
-    Public Shared Function Reverse(ByVal folder As String, ByVal document As XmlDocument) As Boolean
+    Public Shared Function Reverse(ByVal folder As String, ByVal document As XmlDocument, _
+                                   Optional ByVal bRecursive As Boolean = True) As Boolean
         Dim bResult As String = False
         Try
-            document.AppendChild(document.CreateNode(XmlNodeType.Element, "root", ""))
-            bResult = LoadFolders(folder, document.DocumentElement)
+            document.AppendChild(document.CreateNode(XmlNodeType.Element, "project", ""))
+            bResult = LoadFolders(folder, document.DocumentElement, bRecursive)
             If bResult Then document.Save(My.Computer.FileSystem.CombinePath(folder, "example.xml"))
 
         Catch ex As Exception
@@ -20,23 +21,26 @@ Public Class VbCodeReverse
 
 #Region "Private methods"
 
-    Private Shared Function LoadFolders(ByVal folder As String, ByVal package As XmlNode) As Boolean
+    Private Shared Function LoadFolders(ByVal folder As String, ByVal package As XmlNode, ByVal bRecursive As Boolean) As Boolean
 
         Dim bResult As String = False
         Try
             Dim child As XmlNode
 
+            package.Attributes.SetNamedItem(package.OwnerDocument.CreateAttribute("folder"))
+            package.Attributes.GetNamedItem("folder").Value = folder
+
             For Each foundFile As String In My.Computer.FileSystem.GetFiles(folder, FileIO.SearchOption.SearchTopLevelOnly, "*.vb")
                 LoadFile(foundFile, package)
             Next
+            If bRecursive Then
+                For Each directory As String In My.Computer.FileSystem.GetDirectories(folder)
 
-            For Each directory As String In My.Computer.FileSystem.GetDirectories(folder)
+                    child = package.AppendChild(package.OwnerDocument.CreateNode(XmlNodeType.Element, "package", ""))
 
-                child = package.AppendChild(package.OwnerDocument.CreateNode(XmlNodeType.Element, "package", ""))
-
-                LoadFolders(My.Computer.FileSystem.CombinePath(folder, directory), child)
-            Next
-
+                    LoadFolders(My.Computer.FileSystem.CombinePath(folder, directory), child, bRecursive)
+                Next
+            End If
             bResult = True
 
         Catch ex As Exception
