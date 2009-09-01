59c26cd8a772376568e3477d58d444eae0c74141
c_comte@club-internet.fr <c_comte@club-internet.fr@4dde9020-e581-11dd-a893-8d7ce9fbef62>
2009-09-01 21:21:49 +0000

    Fix: check prototypes loading errors.

--- ClassXmlProject/xml/XmlNodeManager.vb
+++ ClassXmlProject/xml/XmlNodeManager.vb
@@ -29,145 +29,126 @@ Public Class XmlNodeManager
 
 #Region "Prototype pattern"
 
-    Public Function AddView(ByVal strName As String, ByRef component As Object) As Boolean
-        Try
-            m_xmlListViews.Add(strName, component)
+    Public Function CheckPrototypes() As Boolean
+        If m_xmlListViews.Count = 0 Or m_xmlListDocuments.Count = 0 Then
+            m_xmlListViews.Clear()
+            m_xmlListDocuments.Clear()
+        End If
+    End Function
 
-        Catch ex As Exception
-            Throw ex
-        End Try
+    Public Function AddView(ByVal strName As String, ByRef component As Object) As Boolean
+
+        m_xmlListViews.Add(strName, component)
     End Function
 
     Public Function AddDocument(ByVal strName As String, ByRef component As Object) As Boolean
-        Try
-            m_xmlListDocuments.Add(strName, component)
 
-        Catch ex As Exception
-            Throw ex
-        End Try
+        m_xmlListDocuments.Add(strName, component)
     End Function
 
     Private Function GetView(ByVal strViewName As String) As XmlComponent
         Dim xmlResult As XmlComponent = Nothing
-        Try
-            xmlResult = FindView(strViewName)
-            If xmlResult Is Nothing Then
-                Throw New Exception("Try to get an undefined view : '" + strViewName + "'")
-            End If
-        Catch ex As Exception
-            Throw ex
-        End Try
+
+        xmlResult = FindView(strViewName)
+        If xmlResult Is Nothing Then
+            Throw New Exception("Try to get an undefined view : '" + strViewName + "'")
+        End If
+
         Return xmlResult
     End Function
 
     Public Function CreateForm(ByVal document As XmlComponent) As Form
         Dim frmResult As Form = Nothing
-        Try
-            If document Is Nothing Then
-                Throw New Exception("Document must not be null")
-            End If
-            Dim view As XmlComponent = GetView(document.Node.Name)
-            frmResult = CType(view, InterfViewForm).CreateForm(document)
 
-        Catch ex As Exception
-            Throw ex
-        End Try
+        If document Is Nothing Then
+            Throw New Exception("Document must not be null")
+        End If
+        Dim view As XmlComponent = GetView(document.Node.Name)
+        frmResult = CType(view, InterfViewForm).CreateForm(document)
+
         Return frmResult
     End Function
 
     Public Overloads Function CreateView(ByVal strViewName As String, Optional ByVal docXml As XmlDocument = Nothing) As XmlComponent
         Dim xmlResult As XmlComponent = Nothing
-        Try
-            xmlResult = FindView(strViewName)
-            If xmlResult IsNot Nothing Then
-                xmlResult = xmlResult.Clone(Nothing)
-                xmlResult.Document = docXml
-            Else
-                Throw New Exception("Try to create an undefined view : '" + strViewName + "'")
-            End If
-        Catch ex As Exception
-            Throw ex
-        End Try
+
+        xmlResult = FindView(strViewName)
+        If xmlResult IsNot Nothing Then
+            xmlResult = xmlResult.Clone(Nothing)
+            xmlResult.Document = docXml
+        Else
+            Throw New Exception("Try to create an undefined view : '" + strViewName + "'")
+        End If
+
         Return xmlResult
     End Function
 
     Public Overloads Function CreateView(ByVal xmlNode As XmlNode, ByVal strViewName As String, Optional ByVal docXml As XmlDocument = Nothing) As XmlComponent
         Dim xmlResult As XmlComponent = Nothing
-        Try
-            xmlResult = FindView(strViewName)
-            If xmlResult IsNot Nothing Then
-                xmlResult = xmlResult.Clone(xmlNode)
-                xmlResult.Document = docXml
-            Else
-                Throw New Exception("Try to create an undefined view : '" + strViewName + "'")
-            End If
-        Catch ex As Exception
-            Throw ex
-        End Try
+
+        xmlResult = FindView(strViewName)
+        If xmlResult IsNot Nothing Then
+            xmlResult = xmlResult.Clone(xmlNode)
+            xmlResult.Document = docXml
+        Else
+            Throw New Exception("Try to create an undefined view : '" + strViewName + "'")
+        End If
+
         Return xmlResult
     End Function
 
     Public Overloads Function CreateDocument(ByVal strNodeName As String, Optional ByVal docXml As XmlDocument = Nothing, Optional ByVal bLoadChildren As Boolean = False) As XmlComponent
         Dim xmlResult As XmlComponent = Nothing
-        Try
-            xmlResult = FindDocument(strNodeName)
 
-            If xmlResult Is Nothing Then
-                Throw New Exception("Try to create an undefined document : '" + strNodeName + "'")
-            Else
-                xmlResult = xmlResult.Clone(Nothing, bLoadChildren)
-                xmlResult.Document = docXml
-                xmlResult.Node = xmlResult.CreateNode(strNodeName)
-            End If
+        xmlResult = FindDocument(strNodeName)
 
-            xmlResult.SetDefaultValues(True)
+        If xmlResult Is Nothing Then
+            Throw New Exception("Try to create an undefined document : '" + strNodeName + "'")
+        Else
+            xmlResult = xmlResult.Clone(Nothing, bLoadChildren)
+            xmlResult.Document = docXml
+            xmlResult.Node = xmlResult.CreateNode(strNodeName)
+        End If
 
-        Catch ex As Exception
-            Throw ex
-        End Try
+        xmlResult.SetDefaultValues(True)
+
         Return xmlResult
     End Function
 
     Public Overloads Function CreateDocument(ByVal xmlNode As XmlNode, Optional ByVal bLoadChildren As Boolean = False) As XmlComponent
         Dim xmlResult As XmlComponent = Nothing
-        Try
-            If xmlNode IsNot Nothing Then
 
-                xmlResult = FindDocument(xmlNode.Name)
+        If xmlNode IsNot Nothing Then
 
-                If xmlResult Is Nothing Then
-                    Throw New Exception("Try to create an undefined document : " + xmlNode.Name + "'")
-                Else
-                    xmlResult = xmlResult.Clone(xmlNode, bLoadChildren)
-                End If
+            xmlResult = FindDocument(xmlNode.Name)
+
+            If xmlResult Is Nothing Then
+                Throw New Exception("Try to create an undefined document : " + xmlNode.Name + "'")
+            Else
+                xmlResult = xmlResult.Clone(xmlNode, bLoadChildren)
             End If
-        Catch ex As Exception
-            Throw ex
-        End Try
+        End If
+
         Return xmlResult
     End Function
 
     Private Function FindView(ByVal strName As String) As XmlComponent
         Dim xmlResult As XmlComponent = Nothing
-        Try
-            If m_xmlListViews.Contains(strName) Then
-                xmlResult = CType(m_xmlListViews.Item(strName), XmlComponent)
-            End If
-        Catch ex As Exception
-            Throw ex
-        End Try
+
+        If m_xmlListViews.Contains(strName) Then
+            xmlResult = CType(m_xmlListViews.Item(strName), XmlComponent)
+        End If
+
         Return xmlResult
     End Function
 
     Private Function FindDocument(ByVal strName As String) As XmlComponent
         Dim xmlResult As XmlComponent = Nothing
-        Try
-            If m_xmlListDocuments.Contains(strName) Then
-                xmlResult = CType(m_xmlListDocuments.Item(strName), XmlComponent)
-            End If
-        Catch ex As Exception
-            Throw ex
-        End Try
+
+        If m_xmlListDocuments.Contains(strName) Then
+            xmlResult = CType(m_xmlListDocuments.Item(strName), XmlComponent)
+        End If
+
         Return xmlResult
     End Function
 #End Region
